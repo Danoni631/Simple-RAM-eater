3100d64e69f8fba1e860ccfe3081315645b032fd
Danoni631 <danunisonsoi@gmail.com>
2026-06-28 17:32:18 -0300

    Implement RAM_Breaker function for memory allocation
    Added RAM_Breaker function to allocate memory chunks until failure.

--- crash.c
+++ crash.c
@@ -10,3 +10,39 @@ void Defs()
         return 1;
     }
 }
+
+void RAM_Breaker()
+{
+    const size_t chunk_size = 100 * 1024 * 1024;
+    size_t total_allocated = 0;
+
+    while (1)
+    {
+        char *chunk = (char*)malloc(chunk_size);
+        if (!chunk) {
+            fprintf(stderr, "\n[!] Failed to allocate memory!\n");
+            fprintf(stderr, "Max memory allocated before crash: %zu MB\n", total_allocated / (1024 * 1024));
+            break;
+        }
+
+        // Write data in block to force OS to map real memory (RAM/Swap)
+        memset(chunk, 0xAA, chunk_size);
+
+        // Allocate in ray
+        if (size == capacity)
+        {
+            capacity *= 2;
+            char **tmp = realloc(memory_chunks, capacity * sizeof(char*));
+            if (!tmp) {
+                fprintf(stderr, "Failed
+                  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!\n");
+                free(chunk);
+                break;
+            }
+            memory_chunks = tmp;
+        }
+        memory_chunks[size++] = chunk;
+        total_allocated += chunk_size;
+
+        printf("Alocados com sucesso: %zu MB\n", total_allocated / (1024 * 1024));
+}
